11dd99a76f568f42a84c6bf9ab0a76aac8f8bf7f
mcnadia <39189412+mcnadia@users.noreply.github.com>
2019-05-12 16:13:44 +0200

    effacer la fonction  trtReponse

--- loremML.js
+++ loremML.js
@@ -9,8 +9,5 @@ xhr = open('GET','lorem'+n, true );
 xhr.onreadystatechange = trtResponse;
 xhr.send();
 }
- function trtResponse(){
- if (xhr.status == 200 && xhr.readyState == 4){
-   document.getElementById('ipsum').innerHTML = xhr.responseText;
- }  
+ 
  }
