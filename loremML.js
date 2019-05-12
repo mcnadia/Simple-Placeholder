2c3d90aeadd155189a8032ba23c7fe30b8fc6904
mcnadia <39189412+mcnadia@users.noreply.github.com>
2019-05-12 16:01:43 +0200

    fichier javascript

---
+++ loremML.js
@@ -0,0 +1,32 @@
+﻿var langues = {   FR : 'français', 
+  EN : 'english', 
+  NL : 'nederlands', 
+  DE : 'deutsch' 
+}; 
+var xhr ;
+function showBox(n) { 
+xhr = open('GET','lorem'+n, true );
+xhr.onreadystatechange = trtResponse;
+xhr.send();
+}
+ function trtResponse(){
+ if (xhr.status == 200 && xhr.readyState == 4){
+   document.getElementById('ipsum').innerHTML = xhr.responseText;
+ }  
+ }
+
+function initPage(){
+xhr = new XMLHttpRequest();
+xhr.open('get', 'getLangues', true);
+xhr.onload = makeSelect;
+xhr.send();
+}
+function makeSelect(){
+var lng = JSON.parse(xhr.responseText);
+var liste = '';
+for(var i in lng){
+liste += '<option value=' + lng[i].id + '>'
++ lng[i].lib + '</option>';
+}
+setElem('lang', liste);
+}
